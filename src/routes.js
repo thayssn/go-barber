410dc022206a28fba3a1bcb0888cf202ce2e1d78
Thays <thays.sn@hotmail.com>
2019-07-04 19:00:10 -0300

    Loading and registering Models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.send('Henlo!'));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Thays dos Santos Neves',
+    email: 'thays.sn@22hotmail.com.br',
+    password_hash: 'H324H23H893H',
+  });
+  return res.json(user);
+});
 
 module.exports = routes;
