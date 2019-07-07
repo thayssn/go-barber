0cabfd366fc6a9025ed4e9e156c6515e36a7d69d
Thays <thays.sn@hotmail.com>
2019-07-07 00:38:19 -0300

    Verify if user exists and create hash for password

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,8 +3,14 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     try {
-      const user = await User.create(req.body);
-      return res.status(201).json(user);
+      const userExists = await User.findOne({
+        where: { email: req.body.email },
+      });
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+      const { id, name, email, provider } = await User.create(req.body);
+      return res.status(201).json({ id, name, email, provider });
     } catch (err) {
       return err;
     }
