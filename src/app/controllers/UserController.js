f17b52bcb18087a72e0a52a7590047e3180b8402
Thays <thays.sn@hotmail.com>
2019-07-07 01:35:08 -0300

    Updating user data

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,6 +17,30 @@ class UserController {
         .json({ error: 'An error ocurred during the register' });
     }
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email: req.body.email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.verifyPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.status(200).json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import AuthController from './app/controllers/AuthController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 const API_BASE_URL = '/api';
 
 routes.post(`${API_BASE_URL}/users`, UserController.store);
 routes.post(`${API_BASE_URL}/auth`, AuthController.store);
+
+routes.use(authMiddleware);
+routes.put(`${API_BASE_URL}/users/`, authMiddleware, UserController.update);
 
 module.exports = routes;
